e2984597eaa9a967810a4649ca22ef51e0d665ac
dezelin <dezelin@e57dd50e-e725-0410-9584-8737329c29b9>
2006-12-31 14:29:13 +0000

    Added a file remotely

---
+++ main/sysdevice.h
@@ -0,0 +1,33 @@
+/*
+    sysdevice.h - Platform dependent device functions.
+    Copyright (C) 2006  Aleksandar Dezelin <dezelin@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _SYSDEVICE_H
+#define _SYSDEVICE_H
+
+#include <stdafx.h>
+
+#include "list.h"
+
+/* Enumerates all supported optical devices */
+int optcl_device_enumerate(optcl_list **devices);
+
+/* Gets info about the specified device */
+int optcl_device_get_info(const char *device_name, optcl_device **device);
+
+#endif /* _SYSDEVICE_H */
