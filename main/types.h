bb75678b855e8190cb22d0a3b0d69c2384e2f507
dezelin <dezelin@e57dd50e-e725-0410-9584-8737329c29b9>
2007-01-23 18:01:38 +0000

    [FEATURE] Added macros to convert from little-endian SCSI values to host endiannes.

--- main/types.h
+++ main/types.h
@@ -31,4 +31,105 @@ typedef unsigned __int32	uint32_t;
 typedef unsigned __int64	uint64_t;
 #endif
 
+/*
+ * Macros to convert from little-endian SCSI values to host endiannes
+ */
+
+#ifdef WIN32
+#define LITTLE_ENDIAN
+#endif
+
+#define uint16_swap_le_be(val)	((uint16_t)(				\
+	(((uint16_t)(val) & (uint16_t)(0x00ffU) << 8)		|	\
+	(((uint16_t)(val) & (uint16_t)(0xff00U) >> 8)))
+
+#define uint32_swap_le_be(val)  ((uint32_t)(				\
+	(((uint32_t)(val) & (uint32_t)(0x000000ffU) << 24)	|	\
+	(((uint32_t)(val) & (uint32_t)(0x0000ff00U) <<  8)	|	\
+	(((uint32_t)(val) & (uint32_t)(0x00ff0000U) >>  8)	|	\
+	(((uint32_t)(val) & (uitn32_t)(0xff000000U) >> 24)))
+
+#define uint64_swap_le_be(val)	((uint64_t)(				\
+	(((uint64_t)(val) & (uint64_t)(0x00000000000000ffU) << 56) |	\
+	(((uint64_t)(val) & (uitn64_t)(0x000000000000ff00U) << 40) |	\
+	(((uint64_t)(val) & (uitn64_t)(0x0000000000ff0000U) << 24) |	\
+	(((uint64_t)(val) & (uitn64_t)(0x00000000ff000000U) <<  8) |	\
+	(((uint64_t)(val) & (uitn64_t)(0x000000ff00000000U) >>  8) |	\
+	(((uint64_t)(val) & (uitn64_t)(0x0000ff0000000000U) >> 24) |	\
+	(((uint64_t)(val) & (uitn64_t)(0x00ff000000000000U) >> 40) |	\
+	(((uint64_t)(val) & (uitn64_t)(0xff00000000000000U) >> 56)))
+
+
+#ifdef LITTLE_ENDIAN
+
+#define int16_to_le(val)	((int16_t)(val))
+#define int32_to_le(val)	((int32_t)(val))
+#define int64_to_le(val)	((int64_t)(val))
+
+#define uint16_to_le(val)	((uint16_t)(val))
+#define uint32_to_le(val)	((uint32_t)(val))
+#define uint64_to_le(val)	((uint64_t)(val))
+
+#define int16_from_le(val)	((int16_t)(val))
+#define int32_from_le(val)	((int32_t)(val))
+#define int64_from_le(val)	((int64_t)(val))
+
+#define uint16_from_le(val)	((uint16_t)(val))
+#define uint32_from_le(val)	((uint32_t)(val))
+#define uint64_from_le(val)	((uint64_t)(val))
+
+#define int16_to_be(val)	((int16_t)uint16_swap_le_be(val))
+#define int32_to_be(val)	((int32_t)uint32_swap_le_be(val))
+#define int64_to_be(val)	((int64_t)uint64_swap_le_be(val))
+
+#define uint16_to_be(val)	((uint16_t)uint16_swap_le_be(val))
+#define uint32_to_be(val)	((uint32_t)uint32_swap_le_be(val))
+#define uint64_to_be(val)	((uint64_t)uint64_swap_le_be(val))
+
+#define int16_from_be(val)	((int16_t)uint16_swap_le_be(val))
+#define int32_from_be(val)	((int32_t)uint32_swap_le_be(val))
+#define int64_from_be(val)	((int64_t)uint64_swap_le_be(val))
+
+#define uint16_from_be(val)	((uint16_t)uint16_swap_le_be(val))
+#define uint32_from_be(val)	((uint32_t)uint32_swap_le_be(val))
+#define uint64_from_be(val)	((uint64_t)uint64_swap_le_be(val))
+
+#else
+
+#define int16_to_le(val)	((int16_t)uint16_swap_le_be(val))
+#define int32_to_le(val)	((int32_t)uint32_swap_le_be(val))
+#define int64_to_le(val)	((int64_t)uint64_swap_le_be(val))
+
+#define uint16_to_le(val)	((uint16_t)uint16_swap_le_be(val))
+#define uint32_to_le(val)	((uint32_t)uint32_swap_le_be(val))
+#define uint64_to_le(val)	((uint64_t)uint64_swap_le_be(val))
+
+#define int16_from_le(val)	((int16_t)uint16_swap_le_be(val))
+#define int32_from_le(val)	((int32_t)uint32_swap_le_be(val))
+#define int64_from_le(val)	((int64_t)uint64_swap_le_be(val))
+
+#define uint16_from_le(val)	((uint16_t)uint16_swap_le_be(val))
+#define uint32_from_le(val)	((uint32_t)uint32_swap_le_be(val))
+#define uint64_from_le(val)	((uint64_t)uint64_swap_le_be(val))
+
+#define int16_to_be(val)	((int16_t)(val))
+#define int32_to_be(val)	((int32_t)(val))
+#define int64_to_be(val)	((int64_t)(val))
+
+#define uint16_to_be(val)	((uint16_t)(val))
+#define uint32_to_be(val)	((uint32_t)(val))
+#define uint64_to_be(val)	((uint64_t)(val))
+
+#define int16_from_be(val)	((int16_t)(val))
+#define int32_from_be(val)	((int32_t)(val))
+#define int64_from_be(val)	((int64_t)(val))
+
+#define uint16_from_be(val)	((uint16_t)(val))
+#define uint32_from_be(val)	((uint32_t)(val))
+#define uint64_from_be(val)	((uint64_t)(val))
+
+#endif
+
+
+
 #endif /* _TYPES_H */
