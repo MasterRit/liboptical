32b36e47438d900e9a46a2f18df172d23ef13585
dezelin <dezelin@e57dd50e-e725-0410-9584-8737329c29b9>
2007-01-27 18:14:42 +0000

    [CODING STYLE] Don't define entities with linkage in a header file.

--- main/list.h
+++ main/list.h
@@ -34,87 +34,91 @@ typedef int (*optcl_list_equalfn)(const void *left, const void *right);
 
 
 /* Add new element to head of the list */
-int optcl_list_add_head(optcl_list *list, const void *data);
+extern int optcl_list_add_head(optcl_list *list, const void *data);
 
 /* Add new element to tail of the list */
-int optcl_list_add_tail(optcl_list *list, const void *data);
+extern int optcl_list_add_tail(optcl_list *list, const void *data);
 
 /* Append list to another */
-int optcl_list_append(optcl_list *dest, const optcl_list *src);
+extern int optcl_list_append(optcl_list *dest, const optcl_list *src);
 
 /* Create new list */
-int optcl_list_create(const optcl_list_equalfn equalfn, 
-		      optcl_list **list);
+extern int optcl_list_create(const optcl_list_equalfn equalfn, 
+			     optcl_list **list);
 
 /* Clear list (removes all elements) */
-int optcl_list_clear(optcl_list *list, int deallocate);
+extern int optcl_list_clear(optcl_list *list, int deallocate);
 
 /* Copy all elements from src to dest list */
-int optcl_list_copy(optcl_list *dest, const optcl_list *src);
+extern int optcl_list_copy(optcl_list *dest, const optcl_list *src);
 
 /* Destroy list */
-int optcl_list_destroy(optcl_list *list, int deallocate);
+extern int optcl_list_destroy(optcl_list *list, int deallocate);
 
 /* Find element position */
-int optcl_list_find(const optcl_list *list, 
-		    const void *data, 
-		    optcl_list_iterator *pos);
+extern int optcl_list_find(const optcl_list *list, 
+			   const void *data, 
+			   optcl_list_iterator *pos);
 
 /* Get element at index */
-int optcl_list_get_at_index(const optcl_list *list, int index, void **data);
+extern int optcl_list_get_at_index(const optcl_list *list, 
+				   int index, 
+				   void **data);
 
 /* Get element at iterator position */
-int optcl_list_get_at_pos(const optcl_list *list, 
-			  const optcl_list_iterator pos, 
-			  const void **data);
+extern int optcl_list_get_at_pos(const optcl_list *list, 
+				 const optcl_list_iterator pos, 
+				 const void **data);
 
 /* Get list element equalfn function */
-int optcl_list_get_equalfn(const optcl_list *list, 
-			   optcl_list_equalfn *equalfn);
+extern int optcl_list_get_equalfn(const optcl_list *list, 
+				  optcl_list_equalfn *equalfn);
 
 /* Get element count */
-int optcl_list_get_count(const optcl_list *list, int *count);
+extern int optcl_list_get_count(const optcl_list *list, int *count);
 
 /* Get start position iterator */
-int optcl_list_get_head_pos(const optcl_list *list, optcl_list_iterator *pos);
+extern int optcl_list_get_head_pos(const optcl_list *list, 
+				   optcl_list_iterator *pos);
 
 /* Get end position iterator */
-int optcl_list_get_tail_pos(const optcl_list *list, optcl_list_iterator *pos);
+extern int optcl_list_get_tail_pos(const optcl_list *list, 
+				   optcl_list_iterator *pos);
 
 // Get next element in the list
-int optcl_list_get_next(const optcl_list *list,
-			const optcl_list_iterator pos,
-			optcl_list_iterator *next);
+extern int optcl_list_get_next(const optcl_list *list,
+			       const optcl_list_iterator pos,
+			       optcl_list_iterator *next);
 
 // Get previous element in the list
-int optcl_list_get_previous(const optcl_list *list,
-			    const optcl_list_iterator pos,
-			    optcl_list_iterator *previous);
+extern int optcl_list_get_previous(const optcl_list *list,
+				   const optcl_list_iterator pos,
+				   optcl_list_iterator *previous);
 
 /* Insert new element after iterator position */
-int optcl_list_insert_after(const optcl_list *list, 
-			    const optcl_list_iterator pos, 
-			    const void *data);
+extern int optcl_list_insert_after(const optcl_list *list, 
+				   const optcl_list_iterator pos, 
+				   const void *data);
 
 /* Insert new element before iterator position */
-int optcl_list_insert_before(const optcl_list *list, 
-			     const optcl_list_iterator pos, 
-			     const void *data);
+extern int optcl_list_insert_before(const optcl_list *list, 
+				    const optcl_list_iterator pos, 
+				    const void *data);
 
 /* Remove element from the list */
-int optcl_list_remove(optcl_list *list, optcl_list_iterator pos);
+extern int optcl_list_remove(optcl_list *list, optcl_list_iterator pos);
 
 /* Set element at iterator position */
-int optcl_list_set_at_pos(const optcl_list *list, 
-			  optcl_list_iterator pos, 
-			  const void *element
+extern int optcl_list_set_at_pos(const optcl_list *list, 
+				 optcl_list_iterator pos, 
+				 const void *element
 			  );
 
 /* Set list element equalfn */
-int optcl_list_set_equalfn(optcl_list *list, 
-			   optcl_list_equalfn equalfn);
+extern int optcl_list_set_equalfn(optcl_list *list, 
+				  optcl_list_equalfn equalfn);
 
 /* Sort */
-int optcl_list_sort(optcl_list *list);
+extern int optcl_list_sort(optcl_list *list);
 
 #endif /* _LIST_H */
